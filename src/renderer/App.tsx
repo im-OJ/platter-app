c047677f641ce8f7db043e0f82d1345cec650988
im-OJ <ollie2997@gmail.com>
2020-04-14 21:24:25 +0100

    Add styled components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import { Button } from "antd";
+import styled from "styled-components";
 
 import "antd/dist/antd.css";
 export const App = () => {
   return (
     <p>
-      Hello world <Button>Test</Button>
+      <Shadow>
+        Hello world <Button>Test</Button>
+      </Shadow>
     </p>
   );
 };
+
+const Shadow = styled.div`
+  border-radius: 0px;
+  background: #55b9f3;
+  box-shadow: 12px 12px 24px #3d85af, -12px -12px 24px #6dedff;
+`;
