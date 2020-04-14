f1a7701af10b8309747b923e4a8a5ffa71dd2701
Ollie James <oliver.james@candide.eu>
2020-04-14 01:03:03 +0100

    Remove auto update

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from "electron";
 declare var MAIN_WINDOW_WEBPACK_ENTRY: any;
 
-require("update-electron-app")();
+// require("update-electron-app")();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
