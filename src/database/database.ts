96a7961090284aa0dbb59dcc6ff4e104e679a7f4
im-OJ <ollie2997@gmail.com>
2020-04-15 00:24:56 +0100

    Use sync'd local DB

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,18 +4,25 @@ import PouchDB from "pouchdb-browser";
 import _ from "lodash";
 
 const database = {
-  samples: new PouchDB(config.dbUrl + "samples"),
+  samples: {
+    remote: new PouchDB(config.dbUrl + "samples"),
+    local: new PouchDB("samples"),
+  },
 };
 
 export const getSampleById = (id: string): Promise<Sample> => {
-  return database.samples.get(id);
+  return database.samples.local.get(id);
 };
 
+export const reSyncDBs = () => {
+  console.log("Re syncing databases");
+  database.samples.local.replicate.from(database.samples.remote);
+};
 export const getSamples = async (): Promise<Array<Sample>> => {
+  reSyncDBs();
   console.log("getting sample docs", config.dbUrl + "samples");
-  const all = await database.samples.allDocs({ include_docs: true });
+  const all = await database.samples.local.allDocs({ include_docs: true });
   const samples: Array<any> = [];
-  console.log("got all: " + JSON.stringify(all, null, 1));
   all.rows.map((s) => {
     const sample = s.doc;
     samples.push(sample);
