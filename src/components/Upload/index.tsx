cd64773171b9fef654aab158587660ce26976f08
im-OJ <ollie2997@gmail.com>
2020-05-03 21:34:52 +0100

    Add some upload components

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 import { Page } from "../Page";
-
+import Dragger from "antd/lib/upload/Dragger";
+import { InboxOutlined } from "@ant-design/icons";
 export const Upload = () => {
   return (
     <Page>
-      <>Hello Upload</>
+      <Dragger name="file" multiple={true}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading
+          company data or other band files
+        </p>
+      </Dragger>
     </Page>
   );
 };
